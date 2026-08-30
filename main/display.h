22389ce048c6ccf45cb008e4286a9dae62568782
Tea179 <lukaszrzepk0@gmail.com>
2026-08-30 17:26:16 +0200

    main

--- main/display.h
+++ main/display.h
@@ -1,6 +1,10 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <Adafruit_SSD1306.h>
+
+extern Adafruit_SSD1306 display;
+
 void initDisplay();
 void clearDisplay();
 void renderFrame();
--- main/main.ino
+++ main/main.ino
@@ -8,10 +8,16 @@
 enum GameState {MENU, PONG, SNAKE, TETRIS, FLAPPY_BIRD};
 GameState state = MENU;
 
+int yPong = 0;
+int ySnake = 15;
+int yTetris = 30;
+int yFlappyBird = 45;
+int y = yPong;
+
 void setup() {
     initDisplay();
     initInput();
-    display.setTextSize(2);
+    display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
 }
 
@@ -33,10 +39,55 @@ void loop() {
             break;
     }
     // Selecting
-    clearDisplay();
-    display.print("PONG");
-    display.print("SNAKE");
-    display.print("TETRIS");
-    display.print("FLAPPY_BIRD");
-    renderFrame();
+    if (y = yPong) {
+        clearDisplay();
+        display.setCursor(10, yPong);
+        display.print("PONG <--");
+        display.setCursor(10, ySnake);
+        display.print("SNAKE");
+        display.setCursor(10, yTetris);
+        display.print("TETRIS");
+        display.setCursor(10, yFlappyBird);
+        display.print("FLAPPY_BIRD");
+        renderFrame();
+    }
+    else if (y == yPong && buttonPressed(5)) {
+        y = ySnake;
+        clearDisplay();
+        display.setCursor(10, yPong);
+        display.print("PONG");
+        display.setCursor(10, ySnake);
+        display.print("SNAKE <--");
+        display.setCursor(10, yTetris);
+        display.print("TETRIS");
+        display.setCursor(10, yFlappyBird);
+        display.print("FLAPPY_BIRD");
+        renderFrame();
+    }
+    else if (y == ySnake && buttonPressed(5)) {
+        y = yTetris;
+        clearDisplay();
+        display.setCursor(10, yPong);
+        display.print("PONG");
+        display.setCursor(10, ySnake);
+        display.print("SNAKE");
+        display.setCursor(10, yTetris);
+        display.print("TETRIS <--");
+        display.setCursor(10, yFlappyBird);
+        display.print("FLAPPY_BIRD");
+        renderFrame();
+    }
+    else if (y == yTetris && buttonPressed(5)) {
+        y = yFlappyBird;
+        clearDisplay();
+        display.setCursor(10, yPong);
+        display.print("PONG");
+        display.setCursor(10, ySnake);
+        display.print("SNAKE <--");
+        display.setCursor(10, yTetris);
+        display.print("TETRIS");
+        display.setCursor(10, yFlappyBird);
+        display.print("FLAPPY_BIRD <--");
+        renderFrame();
+    }
 }
